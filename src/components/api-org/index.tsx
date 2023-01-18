43133a0d63c0afe13a8be4cb579586a68a1430e0
fabianeromano <fabian.ed.romano@gmail.com>
2023-01-17 22:59:12 -0300

    feat: agregando datos excel

---
+++ src/components/api-org/index.tsx
@@ -0,0 +1,21 @@
+import { TreeNode } from "@/lib/model";
+import axios from "axios";
+import Papa from "papaparse"
+
+export default{
+    list: async (): Promise<TreeNode[]> => {
+    return axios.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQOlpVb0YTz0GV3LHdiAg49T1Pz4umi5tGb64sJ0aNH8_If16mohnOAUzOmN55tbvXM6xBo1TvcbPqH/pub?output=csv",
+    {
+        responseType: "blob"
+    }
+    ).then(response =>{
+        return new Promise<TreeNode[]>((resolve, reject) =>{
+            Papa.parse(response.data,{
+                header:true,
+                complete: results=> resolve(results.data as TreeNode[]),
+                error: (error)=>reject(error.message)
+                })
+        })
+    })
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
+import apiOrg from "@/components/api-org";
 import TreeOrg from "@/components/tree-org";
+import { TreeNode } from "@/lib/model";
 import data from "../data/data.json"
 
 
 export default function Home() {
-  const datos:any= data
+
+const datosorg = async ()=>{
+    const organigrama = await apiOrg.list()    
+    return organigrama
+  }
+
+const dat = datosorg();
+console.log(dat);
+
+
+const datos:any= data;
 
   return (
     <div className="genealogy-body genealogy-scroll">
         <div className="genealogy-tree">
             <ul>
-              <TreeOrg data={datos}/>
+                <TreeOrg data={datos}/>
             </ul>
         </div>
     </div>
   )
 }
+
